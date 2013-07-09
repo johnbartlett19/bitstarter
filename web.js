8c00919064c2ec7b0181978b45ebf77ded3a437d
Ubuntu <ubuntu@ip-10-164-86-137.ec2.internal>
2013-07-09 12:42:35 +0000

    fix index.html name

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var toSend = fs.readFileSync("index.html'");
+  var toSend = fs.readFileSync("index.html");
   response.send(toSend.toString());
 });
 
